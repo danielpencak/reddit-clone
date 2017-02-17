a2e02503be20d1833b68412c9bafcf01ab44799b
Daniel Pencak <danielpencak@gmail.com>
2017-02-17 10:14:11 -0800

    Adjust db file

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,14 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      database: process.env.DATABASE_URL || 'reddit-clone',
+      database: process.env.DATABASE_URL || 'reddit-clone'
     }
   },
 
   test: {
     client: 'pg',
     connection: {
-      database: process.env.DATABASE_URL || 'reddit-clone-test',
+      database: process.env.DATABASE_URL || 'reddit-clone-test'
     }
   },
 
